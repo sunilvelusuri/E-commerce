458a72bb34e7d95b75b26048047e2257d932c790
sunilvelusuri07 <velusurisunilkumar@gmail.com>
2024-11-12 13:06:12 +0530

    Order service is created and all api endpoints are working fine

---
+++ order-service/src/main/java/com/codechunk/microservices/order/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.codechunk.microservices.order.controller;
+
+import com.codechunk.microservices.order.dto.OrderRequest;
+import com.codechunk.microservices.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order placed successfully";
+    }
+}
---
+++ order-service/src/main/java/com/codechunk/microservices/order/service/OrderService.java
@@ -0,0 +1,28 @@
+package com.codechunk.microservices.order.service;
+
+import com.codechunk.microservices.order.dto.OrderRequest;
+import com.codechunk.microservices.order.model.Order;
+import com.codechunk.microservices.order.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
+}
